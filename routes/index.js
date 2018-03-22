c70363f7c013153be769729206b189ae6fb0b6f9
doug-salvati <dougiesal@gmail.com>
2018-03-22 12:34:00 -0400

    Set up editor files and jQuery

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Resume Bae' });
+  res.render('index', { title: 'Résumé Bae' });
 });
 
 module.exports = router;
