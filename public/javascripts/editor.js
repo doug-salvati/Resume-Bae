9d6e742fd5c1adacc2d7e90bd8ed3840a7875c27
doug-salvati <dougiesal@gmail.com>
2018-03-23 17:41:42 -0400

    Connect data to visuals

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -6,6 +6,7 @@ class Block {
     constructor(height, width) {
         this.height = height;
         this.width = width;
+        this.contents = "";
     }
 }
 
@@ -19,9 +20,28 @@ class Resume {
         this.rows = [[new Block(this.default_block_height, this.max_width)]];
     }
 
+    /* Draw a row */
+    drawPage(selection) {
+        $("#page").html("");
+        for (var i = 0; i < this.rows.length; ++i) {
+            for (var j = 0; j < this.rows[i].length; ++j) {
+                var block = $('<textarea class="block">' + this.rows[i][j].contents + '</textarea>');
+                if (i == selection[0] && j == selection[1]) {
+                    block.addClass("current");
+                }
+                block.data("row", i).data("column", j);
+                block.click(changeSelection);
+                block.change(textChanged);
+                block.css("height", this.rows[i][j].height);
+                block.css("width", this.rows[i][j].width - 8);
+                $("#page").append(block);
+            }
+        }
+    }
+
     /* Add a block to the page at the bottom*/
     add_block_vertical(row) {
-        if (this.rows.length >= max_rows) {
+        if (this.available_height <= 0) {
             alert("No room to add a block");
             return;
         }
@@ -32,7 +52,9 @@ class Resume {
             var height = this.default_block_height;
         }
 
-        this.rows.splice(row + 1, 0, [new Block(height, this.width)]);
+        this.available_height -= height;
+
+        this.rows.splice(row + 1, 0, [new Block(height, this.max_width)]);
     }
 
     /* Add a block to the page at the end of the current road*/
@@ -41,26 +63,28 @@ class Resume {
             alert("No room to add a block");
             return;
         }
-        var height = rows[0].height;
-        var width = this.max_width / (this.rows[row].length + 1);
-        this.rows[row][this.rows[row].length] -= width;
-        this.rows[row].append(new Block(height, width));
+        var height = Math.floor(this.rows[row][0].height);
+        var width = Math.floor(this.rows[row][this.rows[row].length - 1].width / 2);
+        this.rows[row][this.rows[row].length - 1].width -= width;
+        this.rows[row].push(new Block(height, width));
     }
 
     /* Remove a block from the page */
     delete_block(row, column) {
         /* Give width to another block */
         if (column > 0) {
-            this.rows[row][column -1].width += this.rows[row][column].width;
+            this.rows[row][column - 1].width += this.rows[row][column].width;
         } else if (this.rows[row].length > 1) {
-            this.rows[row][this.rows[row].length].width += this.rows[row][column].width;
+            this.rows[row][this.rows[row].length - 1].width += this.rows[row][column].width;
         }
 
         /* Remove block from array */
-        this.rows[row].splice(col, 1);
+        this.rows[row].splice(column, 1);
 
         /* If the row is empty, delete it */
-        this.rows.splice(row, 1);
+        if (this.rows[row].length == 1) {
+            this.rows.splice(row, 1);
+        }
     }
 
     /* Change position of block in the document */
@@ -95,25 +119,47 @@ class Resume {
 
 }
 /* Init the resume element and the event handlers for the buttons */
-function initialize(){
-    var my_resume = new Resume();
+function initialize() {
+    my_resume = new Resume();
+    selection = [0,0] /* Row, column */
+    my_resume.drawPage(selection);
+    $(".block").addClass("current");
+
     $("#add_vertical").click(function() {
-        my_resume.add_block_vertical(my_resume.rows[ROW].length + 1); //ROW is the current row, needs change to element
-    })  
+        my_resume.add_block_vertical(my_resume.rows[selection[0]].length + 1);
+        my_resume.drawPage(selection);
+    });  
     $("#add_horizontal").click(function(){
-        my_resume.add_block_horizontal(my_resume.rows[ROW].length); //ROW is the current row, needs change to element
-    })
+        my_resume.add_block_horizontal(selection[0]);
+        my_resume.drawPage(selection);
+    });
     $("#delete").click(function(){
-        my_resume.delete_block(my_resume.rows[ROW].length, my_resume.rows[ROW][COLUMNS]); //ROW is the current row, needs change to element, COLUMNS same
-    })
-    $("#resize_vertical")/* drag event */ (function(){
+        my_resume.delete_block(selection[0], selection[1]);
+        my_resume.drawPage(selection);
+    });
+
+    /*$("#resize_vertical")/* drag event / (function(){
         var press; //y coord on mouse press
         var release; //y coord on mouse release
         myResume.resize_vertical(my_resume.rows[ROW].length, (release - press))
     })
-    $("#resize_horizontal")/* drag event */ (function(){
+    $("#resize_horizontal")/* drag event / (function(){
         var press; //y coord on mouse press
         var release; //y coord on mouse release
         myResume.resize_horizontal(my_resume.rows[ROW].length, (release - press))
-    })
+    })*/
 }
+
+function changeSelection() {
+    selection = [$(this).data("row"), $(this).data("column")];
+    $(".current").removeClass("current");
+    $(this).addClass("current");
+}
+
+function textChanged() {
+    text = $(this).val();
+    block = my_resume.rows[$(this).data("row")][$(this).data("column")];
+    block.contents = text;
+}
+
+$(document).ready(initialize);
